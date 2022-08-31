d92c6daef1f2253c873b4c3fbf80b7fb389ae47f
Pranay Roni <pranay.roni@icloud.com>
2022-08-31 04:18:34 -0400

    added parallax images
    - new js script to get mouse movement and parallax images of class type layer
    - imported 12 images for main screen

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let intro = document.querySelector(".intro");
 let logo = document.querySelector(".logo-header");
 let logoSpan = document.querySelectorAll(".logo");
-
+let layer = document.querySelectorAll(".layer");
 window.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     logoSpan.forEach((span, idx) => {
