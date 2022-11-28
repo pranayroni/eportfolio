2bf1854672b180f87d572115226dcc42504aa403
Pranay Roni <pranay.roni@icloud.com>
2022-11-28 13:29:20 -0500

    added scrolljs

---
+++ index.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.footer__copyright', { reset: true, scale: .25 })
+ScrollReveal().reveal('.footer__social--list', { reset: true, scale: .25 })
+ScrollReveal().reveal('.topnav', { delay: 100, reset: true, scale: .5 })
+ScrollReveal().reveal('#languages', { delay: 250, reset: true })
+ScrollReveal().reveal('.project', { delay: 250, reset: true, scale: .85 })
+ScrollReveal().reveal('.about-me__info', { delay: 250, reset: true, scale: .85 })
+ScrollReveal().reveal('.row', { delay: 50, reset: true, scale: .9 })
