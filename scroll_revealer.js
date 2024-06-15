0b1aa621b798ee5e48e6c6c89147b7be53fdf2eb
Pranay Roni <pranay.roni@icloud.com>
2024-06-15 13:05:09 -0400

    updated scroll revealer

--- scroll_revealer.js
+++ scroll_revealer.js
@@ -1,11 +1,10 @@
-ScrollReveal().reveal('.footer__copyright', { reset: true, scale: .25 })
-ScrollReveal().reveal('.footer__social--list', { reset: true, scale: .25 })
-ScrollReveal().reveal('.topnav', { delay: 100, reset: true, scale: .5 })
+
+ScrollReveal().reveal('.topnav', { delay: 50, reset: true, scale: .85 });
 ScrollReveal().reveal('#languages', { delay: 250, reset: true })
 ScrollReveal().reveal('.project', { delay: 250, reset: true, scale: .85 })
-ScrollReveal().reveal('.about-me__info', { delay: 250, reset: true, scale: .85 })
-ScrollReveal().reveal('.row', { delay: 50, reset: true, scale: .9 })
+ScrollReveal().reveal('.about-me__info', { delay: 50, reset: true, scale: .95 })
 ScrollReveal().reveal('#about-me', { delay: 100, reset: false, scale: .25 })
+// shapes
 ScrollReveal().reveal('#crescentico', { delay: 100, reset: false, scale: .5 })
 ScrollReveal().reveal('#heartico', { delay: 350, reset: false, scale: .5 })
 ScrollReveal().reveal('#dbico', { delay: 450, reset: false, scale: .5 })
