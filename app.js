9c7e8fad0ae5d0d41638f22fdf790a9a17c63991
Pranay Roni <pranay.roni@icloud.com>
2024-06-15 18:15:40 -0400

    refactor: removed unnec code

--- app.js
+++ app.js
@@ -2,14 +2,7 @@ let intro = document.querySelector(".intro");
 let logo = document.querySelector(".logo-header");
 let logoSpan = document.querySelectorAll(".logo");
 let layer = document.querySelectorAll(".layer");
-window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    logoSpan.forEach((span, idx) => {
-      span.classList.add("active");
-    }, (idx + 1)*(400));
-  });
-  
-});
+
 
 window.addEventListener('resize', function() {
   let personalLogo = document.querySelectorAll('.personal__logo');
