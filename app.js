fee9021d2f8cae0dedc17b5bc8545dd2b5865248
Pranay Roni <pranay.roni@icloud.com>
2024-06-15 12:22:17 -0400

    style:
    - Update profile picture border radius, box shadow, and dark mode styles for shadows
    
    - shadow is pink if light

--- app.js
+++ app.js
@@ -6,7 +6,39 @@ window.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     logoSpan.forEach((span, idx) => {
       span.classList.add("active");
-    }, (idx + 1)(400));
+    }, (idx + 1)*(400));
   });
   
 });
+
+// dark mode
+
+var icon = document.getElementById("themebutton");
+const footers = document.getElementsByClassName("footer__social--link");
+let line1 = document.getElementById("line1")
+let line2 = document.getElementById("line2")
+let pfps = document.getElementsByClassName("about-me__picture--mask");
+icon.onclick = function(){
+  document.body.classList.toggle("dark-mode");
+  for(let i = 0; i < pfps.length; i++){
+    pfps[i].classList.toggle("dark-mode-pfp-shadow");
+  }
+  if(document.body.classList.contains("dark-mode")){
+    line1.style.filter = "none"
+    line2.style.filter = "none"
+    
+    icon.src = "./assets/light.png";
+    for(let i = 0; i < footers.length; i++){
+      footers[i].classList.add("link__hover-effect--black");
+      footers[i].classList.remove("link__hover-effect--white");
+    }
+} else{
+  icon.src = "./assets/darkmode.png";
+  line1.style.filter = "invert()"
+  line2.style.filter = "invert()"
+  for(let i = 0; i < footers.length; i++){
+    footers[i].classList.add("link__hover-effect--white");
+    footers[i].classList.remove("link__hover-effect--black");
+  }
+}
+}
